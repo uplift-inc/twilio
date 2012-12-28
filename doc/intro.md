b058d631197b2cc2b6076c34d3319067c5f60180
owainlewis <owain@owainlewis.com>
2012-12-28 18:28:47 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to twillio
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/twillio/core_test.clj
@@ -0,0 +1,7 @@
+(ns twillio.core-test
+  (:use clojure.test
+        twillio.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
