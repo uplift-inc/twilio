1dafe8aff4b7809a62c08bb97742e149494b47e5
Mahe <mahesh@uplift.com>
2015-05-26 16:24:13 -0700

    Changed http-kit to cli-http in dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.1.18"]])
+                 [clj-http "1.1.1"]])
